110437ecf04d0ee8adc352d00e0800d09b035920
Szakker123 <szakonyi.adam.bosco@gmail.com>
2023-09-21 21:10:46 +0200

    Harmadik - Negyedik hiba: 8. sorban hiányzik a ; és a 9. sorban nincs befejezve a for ciklus

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[N_ELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa';
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
