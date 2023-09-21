ab6dce365500f59592deb2de521651865e83d3f2
Szakker123 <szakonyi.adam.bosco@gmail.com>
2023-09-21 21:17:34 +0200

    Hetedik - Nyolcadik - Kilencedik hiba: 18. sorban átlagnál inkébb double használata, a for ciklusban az N_ELEMNTS után  , helyett ; kell + 21.sorban hiányzik a ;

--- main.cpp
+++ main.cpp
@@ -15,10 +15,10 @@ int main()
         std::cout << "Ertek:";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    double atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
